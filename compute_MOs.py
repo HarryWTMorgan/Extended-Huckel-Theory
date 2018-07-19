dc768c3ab7551d091b71751533803eb36a7b5dbe
Unknown <harry.morgan@chem.ox.ac.uk>
2018-07-19 17:17:04 +0100

    MOs sorted
    compute_MOs.py includes a function to return energies and MOs in order of energy

--- compute_MOs.py
+++ compute_MOs.py
@@ -7,19 +7,23 @@ import numpy as np
 import scipy.linalg
 #import matplotlib.pyplot as plt
 
-from input_molecule import create_methane
+from ammonia_input import create_ammonia
 from overlap_matrix import construct_overlap_matrix
 from hamiltonian_matrix import compute_hamiltonian_matrix
 
-atoms, positions, basis = create_methane()
+atoms, positions, basis = create_ammonia()
 
 overlap_matrix = construct_overlap_matrix(atoms, positions, basis)
 
 hamiltonian = compute_hamiltonian_matrix(overlap_matrix, basis)
 
-energies, MOs = scipy.linalg.eig(hamiltonian,overlap_matrix)
+def ordered_MOs(H,S):
+    unsorted_energies, unsorted_MOs = scipy.linalg.eig(H,S)
+    indices = np.argsort(unsorted_energies)
+    energies = unsorted_energies[indices]
+    MOs = unsorted_MOs[:,indices]
+    return MOs, energies
+    
 
-ordered_energies = np.sort(energies)
-
-print(ordered_energies)
+MOs, energies = ordered_MOs(hamiltonian,overlap_matrix)
 
